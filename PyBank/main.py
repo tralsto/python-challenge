c899f5fddd50a1a5ce967d5087b32a39c2ded721
Tait Ralston <taitralston@gmail.com>
2023-07-02 14:48:35 -0700

    update push

---
+++ PyBank/main.py
@@ -0,0 +1,22 @@
+#budget_data.csv data set
+
+# Import os and csv modules to import the csv data files
+import os
+import csv
+
+# Open and read csv file
+budget_csv = os.path.join('Resources', 'budget_data.csv')
+
+with open(budget_csv) as csv_file:
+    csv_reader  = csv.reader(csv_file, delimiter=",")
+
+    print(csv_reader)
+
+    # Read header row of csv file
+    csv_header = next(csv_file)
+    print(f"Header: {csv_header}")
+
+    # Then read through every row after the header row
+    for row in csv_reader:
+        print(row)
+
