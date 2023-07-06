c07afbcc74c8ae1dafa52fd66cd8b8aa8406e571
Tait Ralston <taitralston@gmail.com>
2023-07-05 22:04:01 -0700

    upload files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,22 +1,61 @@
-#budget_data.csv data set
+# PyBank dataset
 
 # Import os and csv modules to import the csv data files
 import os
 import csv
 
-# Open and read csv file
-budget_csv = os.path.join('Resources', 'budget_data.csv')
+# Path for csv file
+budget_csv = os.path.join('.', 'Resources', 'budget_data.csv')
 
+# Set variables for use in dataset analysis
+total_months = []
+total_net_amount = 0
+total_change = 0
+change = 0
+average_change = []
+greatest_increase = []
+greatest_decrease = []
+
+# Open csv file containing the financial data
 with open(budget_csv) as csv_file:
     csv_reader  = csv.reader(csv_file, delimiter=",")
-
-    print(csv_reader)
-
-    # Read header row of csv file
-    csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
+    
+    # Skip header so it does not get analyzed
+    header = next(csv_reader)
 
     # Then read through every row after the header row
     for row in csv_reader:
-        print(row)
 
+        # Analyze the total number of months in the dataset
+        date = (row[0])
+        total_months.append(date)
+
+        month_count = len(total_months)
+
+        # Net total amount of "Profit/Losses" over the entire period
+        profit_losses = int(row[1])
+        total_net_amount += profit_losses
+
+        # Changes in "Profit/Losses" over the entire period, and then the average of those changes
+        change_pro_los = profit_losses - change
+        average_change.append(change_pro_los)
+
+        total_avg_change = sum(average_change)/len(average_change)
+        total_avg_change = round(total_avg_change, 2)
+
+        # The greatest increase in profits (date and amount) over the entire period
+        # The greatest decrease in profits (date and amount) over the entire period
+
+# Create the output txt file
+output_txtfile = "budget_analysis.txt"
+
+with open(output_txtfile, 'w') as file:
+
+# Print analysis title and analysis values
+print("Financial Anaysis")
+print("----------------------------")
+print("Total Months: ", month_count) 
+print(f"Total: ${total_net_amount}")
+print(f"Average Change: ${total_avg_change}")
+print(f"Greatest Increase in Profits: {}%")
+print(f"Greatest Decrease in Profits: {}%")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,22 +1,67 @@
-# election_data.csv data set
+# PyPoll dataset
 
 # Import os and csv modules to import the csv data files
 import os
 import csv
 
-# Open and read csv file
-election_csv = os.path.join ('Resources', 'election_data.csv')
+# Path for csv file
+election_csv = os.path.join ('.', 'Resources', 'election_data.csv')
 
+# Variables for use in analysis of dataset
+total_votes = 0
+stockham_votes = 0
+degette_votes = 0
+doane_votes = 0
+
+# Open and read csv file containing election data
 with open(election_csv) as csv_file:
     csv_reader  = csv.reader(csv_file, delimiter=",")
 
-    print(csv_reader)
-
-    # Read header row of csv file
-    csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
+    # Skip header so it does not get analyzed
+    header = next(csv_reader)
 
     # Then read through every row after the header row
     for row in csv_reader:
-        print(row)
 
+        # The total number of votes cast
+        total_votes += 1
+
+        # A complete list of candidates who received votes
+        candidate_name = row[2]
+        
+        # Count the votes for each candidate
+        if candidate_name == "Charles Casper Stockham":
+            stockham_votes += 1
+        elif candidate_name == "Diana DeGette":
+            degette_votes += 1
+        elif candidate_name == "Raymon Anthony Doane":
+            doane_votes += 1
+
+# Calculate the percent of vote per candidate
+stockham_percentage = (stockham_votes / total_votes) * 100
+stockham_percentage = round(stockham_percentage, 3)
+
+degette_percentage = (degette_votes / total_votes) * 100
+degette_percentage = round(degette_percentage, 3)
+
+doane_percentage = (doane_votes / total_votes) * 100
+doane_percentage = round(doane_percentage, 3)
+
+# The winner of the election based on popular vote
+if stockham_votes > degette_votes and stockham_votes > doane_votes:
+    winner = "Charles Casper Stockham"
+elif degette_votes > stockham_votes and degette_votes > doane_votes:
+    winner = "Diana DeGette"
+elif doane_votes > stockham_votes and doane_votes > degette_votes:
+    winner = "Raymon Anthony Doane"
+
+print("Election Results")
+print("----------------------------")
+print(f"Total Votes: {total_votes}")
+print("----------------------------")
+print(f"Charles Casper Stockham: {stockham_percentage}% ({stockham_votes})")
+print(f"Diana DeGette: {degette_percentage}% ({degette_votes})")
+print(f"Raymon Anthony Doane: {doane_percentage}% ({doane_votes})")
+print("----------------------------")
+print(f"Winner: {winner}")
+print("----------------------------")
