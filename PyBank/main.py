cb21e7280e9c8dfa75be1c701b950c2a812424ed
Tait Ralston <taitralston@gmail.com>
2023-07-10 21:59:16 -0700

    commiting new edited code for re submission

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,4 @@
 # PyBank dataset
-
 # Import os and csv modules to import the csv data files
 import os
 import csv
@@ -10,11 +9,8 @@ budget_csv = os.path.join('.', 'Resources', 'budget_data.csv')
 # Set variables for use in dataset analysis
 total_months = []
 total_net_amount = 0
-total_change = 0
-change = 0
-average_change = []
-greatest_increase = ["", 0]
-greatest_decrease = ["", 0]
+change = []
+previous_pnl = 0
 
 # Open csv file containing the financial data
 with open(budget_csv) as csv_file:
@@ -30,34 +26,31 @@ with open(budget_csv) as csv_file:
         date = (row[0])
         total_months.append(date)
 
-        month_count = len(total_months)
-
         # Net total amount of "Profit/Losses" over the entire period
         profit_losses = int(row[1])
         total_net_amount += profit_losses
 
-        # Changes in "Profit/Losses" over the entire period, and then the average of those changes
-        change_profit_loss = profit_losses - change
-        average_change.append(change_profit_loss)
+        # Changes in "Profit/Losses" over the entire period
+        change_profit_loss = profit_losses - previous_pnl   
+        change.append(change_profit_loss)
+        previous_pnl = profit_losses
 
-        total_avg_change = sum(average_change)/len(average_change)
-        total_avg_change = round(total_avg_change, 2)
+# Calculate total number of months
+month_count = len(total_months)
 
-        # The greatest increase in profits (date and amount) over the entire period
-        if change_profit_loss > greatest_increase[1]:
-            greatest_increase[0] = date
-            greatest_increase[1] = change_profit_loss
-    
-        percent_inc = (greatest_increase[1] / total_net_amount) * 100
-        percent_inc = round(percent_inc, 2)
+# Calculate average change from overall profit/losses
+average_change = sum(change[1:]) / (month_count - 1)
+average_change = round(average_change, 2)
 
-        # The greatest decrease in profits (date and amount) over the entire period
-        if change_profit_loss < greatest_decrease[1]:
-            greatest_decrease[0] = date
-            greatest_decrease[1] = change_profit_loss
-        
-        percent_dec = (greatest_decrease[1] / total_net_amount) * 100
-        percent_dec = round(percent_dec, 2)
+# Utilize max and min functions to find the values over the entire period
+greatest_max = change.index(max(change))
+greatest_min = change.index(min(change))
+
+# Retrieve matching date to max/min values
+max_date = total_months[greatest_max]
+greatest_increase = change[greatest_max]
+min_date = total_months[greatest_min]
+greatest_decrease = change[greatest_min]
 
 # Create the output txt file into the analysis folder
 output_folder = os.path.join('.', 'analysis')
@@ -68,16 +61,15 @@ with open(output_txtfile, 'w') as file:
   file.write("----------------------------\n")
   file.write("Total Months: " + str(month_count) + "\n") 
   file.write("Total: $" + str(total_net_amount) + "\n")
-  file.write("Average Change: $" + str(total_avg_change) + "\n")
-  file.write("Greatest Increase in Profits: " + str(greatest_increase[0]) + str(percent_inc) + "\n")
-  file.write("Greatest Decrease in Profits: " + str(greatest_decrease[0]) + str(percent_dec) + "\n")
-
+  file.write("Average Change: $" + str(average_change) + "\n")
+  file.write("Greatest Increase in Profits: " + str(max_date) + " ($" + str(greatest_increase) + ")" + "\n")
+  file.write("Greatest Decrease in Profits: " + str(min_date) + " $(" + str(greatest_decrease) + ")" + "\n")
 
 # Print analysis title and analysis values in terminal
 print("Financial Anaysis")
 print("----------------------------")
 print("Total Months: ", month_count) 
 print(f"Total: ${total_net_amount}")
-print(f"Average Change: ${total_avg_change}")
-print(f"Greatest Increase in Profits: {greatest_increase[0]} ({percent_inc}%)")
-print(f"Greatest Decrease in Profits: {greatest_decrease[0]} ({percent_dec}%)")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase in Profits: {max_date} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {min_date} (${greatest_decrease})")
